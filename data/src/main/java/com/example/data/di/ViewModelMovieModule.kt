ce134e315a3049fddadb910854262bd6f3f19b22
Anelkad <an_kadyova@kbtu.kz>
2023-12-02 15:18:26 +0600

    [error] removed firebase, hilt error

--- data/src/main/java/com/example/data/di/ViewModelMovieModule.kt
+++ data/src/main/java/com/example/data/di/ViewModelMovieModule.kt
@@ -1,21 +1,19 @@
 package com.example.data.di
 
+import com.example.data.repository.MovieRepositoryImp
 import com.example.data.repository.SavedMovieRepositoryImp
+import com.example.domain.repository.MovieRepository
 import com.example.domain.repository.SavedMovieRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
-@InstallIn(ViewModelComponent::class)
+//@InstallIn(ViewModelComponent::class)
 abstract class ViewModelMovieModule {
-//    @Binds
-//    @ViewModelScoped //жизненный цикл ViewModelScope
-//    abstract fun bindsMovieRepository(imp: MovieRepositoryImp): MovieRepository
+    @Binds //жизненный цикл ViewModelScope
+    abstract fun bindsMovieRepository(imp: MovieRepositoryImp): MovieRepository
 
     @Binds
-    @ViewModelScoped
     abstract fun bindsSavedMovieRepository(imp: SavedMovieRepositoryImp): SavedMovieRepository
 }
